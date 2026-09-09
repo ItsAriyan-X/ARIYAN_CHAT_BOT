12160037150f4e3d34f5faf90ce27652e8eb6898
ItsAriyan-X <sabbir57034@gmail.com>
2026-09-09 22:39:01 +0600

    Enhance logger with error handling and new methods
    Refactor logging functions to include error handling and new log levels.

--- logger/log.js
+++ logger/log.js
@@ -1,42 +1,64 @@
-"use strict";
+const { colors } = require('../func/colors.js');
+const moment = require("moment-timezone");
+const characters = '';
+const getCurrentTime = () => colors.gray(moment().tz("Asia/Ho_Chi_Minh").format("HH:mm:ss DD/MM/YYYY"));
 
-function format(...args) {
-	return args
-		.map(arg => {
-			if (typeof arg === "object" && arg !== null) {
-				try {
-					return JSON.stringify(arg);
-				} catch {
-					return String(arg);
-				}
-			}
-			return String(arg);
-		})
-		.join(" ");
+function logError(prefix, message) {
+	if (message === undefined) {
+		message = prefix;
+		prefix = "ERROR";
+	}
+	console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, message);
+	const error = Object.values(arguments).slice(2);
+	for (let err of error) {
+		if (typeof err == "object" && !err.stack)
+			err = JSON.stringify(err, null, 2);
+		console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, err);
+	}
 }
 
 module.exports = {
-	info(...args) {
-		console.log("[INFO]", format(...args));
+	err: logError,
+	error: logError,
+	warn: function (prefix, message) {
+		if (message === undefined) {
+			message = prefix;
+			prefix = "WARN";
+		}
+		console.log(`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)}`, message);
 	},
-
-	warn(...args) {
-		console.warn("[WARN]", format(...args));
+	info: function (prefix, message) {
+		if (message === undefined) {
+			message = prefix;
+			prefix = "INFO";
+		}
+		console.log(`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)}`, message);
 	},
-
-	error(...args) {
-		console.error("[ERROR]", format(...args));
+	success: function (prefix, message) {
+		if (message === undefined) {
+			message = prefix;
+			prefix = "SUCCES";
+		}
+		console.log(`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)}`, message);
 	},
-
-	success(...args) {
-		console.log("[SUCCESS]", format(...args));
+	master: function (prefix, message) {
+		if (message === undefined) {
+			message = prefix;
+			prefix = "MASTER";
+		}
+		console.log(`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)}`, message);
 	},
-
-	debug(...args) {
-		console.log("[DEBUG]", format(...args));
-	},
-
-	log(...args) {
-		console.log(format(...args));
+	dev: (...args) => {
+		if (["development", "production"].includes(process.env.NODE_ENV) == false)
+			return;
+		try {
+			throw new Error();
+		}
+		catch (err) {
+			const at = err.stack.split('\n')[2];
+			let position = at.slice(at.indexOf(process.cwd()) + process.cwd().length + 1);
+			position.endsWith(')') ? position = position.slice(0, -1) : null;
+			console.log(`\x1b[36m${position} =>\x1b[0m`, ...args);
+		}
 	}
 };
